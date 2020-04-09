a6040519418405f73ce20255a5fc1268e5a8093d
nagireddy panditi <nagireddy.panditi@gmail.com>
2020-04-09 22:25:00 +0530

    added image folder and data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
+      hello
     </div>
   );
 }
-
 export default App;
