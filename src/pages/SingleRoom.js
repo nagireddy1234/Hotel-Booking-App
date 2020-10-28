1eb64f48840d42f8065d993984a0c684d4b82c74
nagiReddy <nagireddy.panditi@gmail.com>
2020-10-28 14:02:32 +0530

    changes made in single room

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -4,6 +4,8 @@ import Hero from '../Components/Hero';
 import Banner from '../Components/Banner';
 import {Link} from 'react-router-dom';
 import {RoomContext} from '../Context';
+import StyledHero from '../Components/styledHero';
+
 
 export default class SingleRoom extends Component {
 
@@ -39,13 +41,47 @@ export default class SingleRoom extends Component {
     // console.log(room)
     return (
       <>
-       <Hero hero="roomsHero">
+       <StyledHero img={images[0]}>
          <Banner title={`${name} room`}>
           <Link to="/rooms" className="btn-primary">
             Back to rooms
           </Link>
          </Banner>
-        </Hero> 
+        </StyledHero> 
+        <section className="single-room">
+          <div className="single-room-images">
+            {defaultImg.map((item, index) => {
+              return <img key={index} src={item} alt={name} />;
+            })}
+          </div>
+          <div className="single-room-info">
+            <article className="desc">
+              <h3>details</h3>
+              <p>{description}</p>
+            </article>
+            <article className="info">
+              <h3>info</h3>
+              <h6>price : ${price}</h6>
+              <h6>size : ${size} SQFT</h6>
+              <h6>
+                max capacity :{" "}
+                {capacity > 1 ? `${capacity} people` : `${capacity} person `}
+              </h6>
+              <h6>{pets ? "pets allowed" : "no pets allowed"}</h6>
+              <h6>{breakfast && "free breakfast included"}</h6>
+            </article>
+          </div>
+        </section>
+        <section className="room-extras">
+          <h6>extras</h6>
+          <ul className="extras">
+            {extras.map((item, index) => {
+              return <li key={index}>- {item}</li>;
+            })}
+          </ul>
+        </section>
+
+        </section>
       </>
     )
   }
